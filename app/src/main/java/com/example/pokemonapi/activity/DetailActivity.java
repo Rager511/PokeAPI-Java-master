64578387b77b9d54c20125c7cdf2657e842f22b8
IMAD MAAILIL <maailil.imad@emsi-edu.ma>
2023-04-16 22:10:31 +0000

    first commit

---
+++ app/src/main/java/com/example/pokemonapi/activity/DetailActivity.java
@@ -0,0 +1,47 @@
+package com.example.pokemonapi.activity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pokemonapi.R;
+import com.example.pokemonapi.databinding.ActivityDetailBinding;
+import com.example.pokemonapi.fragment.DetailFragment;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityDetailBinding activityDetailBinding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(activityDetailBinding.getRoot());
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        if (savedInstanceState == null) {
+            setUpDetailFragment();
+        }
+    }
+
+    private void setUpDetailFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainerView, new DetailFragment())
+                .setReorderingAllowed(true)
+                .commit();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY); //The IMMERSIVE_STICKY use to hide Navigation Bar after short time don't touch on it
+        }
+    }
+}
+//this class sets up the DetailFragment to display the detailed information of a Pokemon and handles hiding the navigation bar for a better user experience.
